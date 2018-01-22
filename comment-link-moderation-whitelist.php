86fc67dd895c29d6348ef81d385f8122a06a97a1
Scott Reilly <scott@coffee2code.com>
2018-01-22 12:10:49 -0700

    Note support back until WP 4.7

--- comment-link-moderation-whitelist.php
+++ comment-link-moderation-whitelist.php
@@ -10,7 +10,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Description: Allows for whitelisted URLs to not count against the comment moderation max number of links limit.
  *
- * Compatible with WordPress 4.8 through 4.9+.
+ * Compatible with WordPress 4.7 through 4.9+.
  *
  * =>> Read the accompanying readme.txt file for instructions and documentation.
  * =>> Also, visit the plugin's homepage for additional information and updates.
